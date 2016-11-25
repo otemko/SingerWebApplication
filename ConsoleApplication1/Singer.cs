5216c30aed2a57bc1c10c1558c4477769f9558e2
otemko <otemko@yandex.ru>
2016-11-25 16:00:17 +0300

    Add read singers\songs with bugs

---
+++ ConsoleApplication1/Singer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Singer
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Biography { get; set; }
+        public int Views { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Song
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Text { get; set; }
+        public int Views { get; set; }
+    }
+}
